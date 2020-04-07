512a64d67e2f6f85a2090aee3675fe5ada1ed842
ronen <ronen567@gmail.com>
2020-04-08 00:15:53 +0300

    Updates

---
+++ precache-manifest.41bafc23fa7a3f67166970621edc6d46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e1fda1661e89468da355161b4f034f",
+    "url": "/smart-brain-client/index.html"
+  },
+  {
+    "revision": "6fde93761bef4a5b771c",
+    "url": "/smart-brain-client/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "fa9c61e2dabe917e78a0",
+    "url": "/smart-brain-client/static/css/main.391ffb7a.chunk.css"
+  },
+  {
+    "revision": "6fde93761bef4a5b771c",
+    "url": "/smart-brain-client/static/js/2.96bfe4e3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/smart-brain-client/static/js/2.96bfe4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa9c61e2dabe917e78a0",
+    "url": "/smart-brain-client/static/js/main.daed7270.chunk.js"
+  },
+  {
+    "revision": "f2d889c97fe231e5b508",
+    "url": "/smart-brain-client/static/js/runtime-main.43ff8180.js"
+  }
+]);
